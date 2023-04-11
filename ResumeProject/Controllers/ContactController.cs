38b43f7fca4932b53a7068c9ff566e23b3502cdb
MuratYucedag <yucedagmurat23@gmail.com>
2023-04-11 23:27:25 +0300

    Add project files.

---
+++ ResumeProject/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResumeProject.Models;
+
+namespace ResumeProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        DbResumeEntities db = new DbResumeEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblContact.ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteMessage(int id)
+        {
+            var values = db.TblContact.Find(id);
+            db.TblContact.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ResumeProject/Controllers/StatisticController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResumeProject.Models;
+
+namespace ResumeProject.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbResumeEntities db = new DbResumeEntities();
+        public ActionResult Index()
+        {
+            //ViewBag.skillCount = db.TblSkill.Count();
+            ViewBag.countProjeTalebi = db.CountProjeTalebi().FirstOrDefault();
+            ViewBag.technologyCount = db.TblTechnology.Count();
+            ViewBag.csharpValue = db.TblTechnology.Where(x => x.TechnologyITitle == "C#").Select(y => y.TechnologyIValue).FirstOrDefault();
+            ViewBag.contactCount = db.TblContact.Count();
+            ViewBag.subjectTesekkur = db.TblContact.Where(x => x.Subject == 1).Count();
+            ViewBag.sumTechnologyValue = db.TblTechnology.Sum(x => x.TechnologyIValue);
+            ViewBag.averageTechnologyValue = db.TblTechnology.Average(x => x.TechnologyIValue);
+            ViewBag.GetBy3ID = db.TblSkill.Where(x => x.SkillID == 3).Select(y => y.SkillTitle).FirstOrDefault();
+            ViewBag.maxTechnologyValue = db.TblTechnology.Max(x => x.TechnologyIValue);
+            return View();
+        }
+    }
+}
